79e86e1e409d279a87ea8870a86daa170619dedf
admin2 <admin2@macbook-pro-admin2.local>
2018-12-18 15:49:53 +0200

    Initial Commit

---
+++ mapPolygonMarker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mapPolygonMarker
+//
+//  Created by admin2 on 12/18/18.
+//  Copyright © 2018 admin2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
